76c74e441773328c91e1368d18cd72345a7a412a
Jamie <jaylovelace42@gmail.com>
2019-12-01 12:24:09 +0000

    login page with css and images

---
+++ src/features/main/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Login.css";
+
+const LoginPage = () => (
+  <div className="login-container">
+    <h1 className="main-heading">a tempo driven playlist generator</h1>
+    <div className="spot-login-btn-div">
+      <button
+        className="btn btn-primary"
+        onClick={() => console.log("clicked")}
+      >
+        Sign in with Spotify
+      </button>
+    </div>
+  </div>
+);
+
+export default LoginPage;
