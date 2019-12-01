31c14fb2b0602d87c1a83670e6ca1d92bc1bf435
Jamie <jaylovelace42@gmail.com>
2019-12-01 12:46:38 +0000

    added router in

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import "./App.css";
-import NavBar from "./features/layout/NavBar";
-import LoginPage from "./features/main/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <LoginPage />
-    </div>
-  );
-}
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false); //eslint-disable-line
+
+  const PrivateRoute = ({ isLoggedIn, ...props }) =>
+    isLoggedIn ? <div>hello</div> : <Redirect to="/login" />;
+
+  return PrivateRoute({ loggedIn });
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import LoginPage from "./features/main/Login";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={LoginPage} />
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
