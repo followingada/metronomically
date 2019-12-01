60f93403cb569d63d8d927342baa36f94cc98f04
Jamie <jaylovelace42@gmail.com>
2019-12-01 13:38:09 +0000

    added title, changed fonts

--- src/features/main/Login.jsx
+++ src/features/main/Login.jsx
@@ -5,7 +5,12 @@ const LoginPage = () => (
   <>
     {/* <NavBar /> */}
     <div className="login-container">
-      <h1 className="main-heading">a tempo driven playlist generator</h1>
+      <h1>
+        {" "}
+        metronomically
+        {/* m&#9679;e&#9679;t&#9679;r&#9679;o&#9679;n&#9679;o&#9679;m&#9679;i&#9679;c&#9679;a&#9679;l&#9679;l&#9679;y */}
+      </h1>
+      <h2 className="main-heading">a tempo driven playlist generator</h2>
       <div className="spot-login-btn-div">
         <button
           className="btn btn-primary"
