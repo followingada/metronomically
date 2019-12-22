ce96df1d0e9d0438ac548ce4fbf9b93e574b1367
Jamie <jaylovelace42@gmail.com>
2019-12-22 16:30:43 +0000

    change login layout

--- src/features/main/Login.jsx
+++ src/features/main/Login.jsx
@@ -4,19 +4,62 @@ import "./Login.css";
 const LoginPage = () => (
   <>
     {/* <NavBar /> */}
-    <div className="login-container">
-      <h1 className="title"> swinglist</h1>
-      <h2 className="main-heading">a tempo driven playlist generator</h2>
-      <div className="spot-login-btn-div">
+    <div
+      style={{
+        height: "100vh",
+        display: "grid",
+        gridTemplateRows: "1fr 1fr",
+        gridTemplateColumns: "1fr 266px 1fr"
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          display: "grid",
+          alignItems: "end"
+        }}
+      >
+        <h1
+          style={{
+            textTransform: "uppercase",
+            // margin: "0 -",
+            marginBottom: "-13px",
+            letterSpacing: "5px",
+            fontSize: "50px"
+          }}
+        >
+          swinglist
+        </h1>
+      </div>
+      <div style={{ backgroundColor: "black", color: "white" }} />
+      <div style={{ backgroundColor: "black", color: "white" }} />
+      <div style={{ position: "absolute", left: "40%", top: "46.5%" }}>
         <button
           className="btn btn-primary"
-          style={{ margin: "5px" }}
+          style={{ width: "266px" }}
           onClick={() => console.log("clicked")}
         >
-          Sign in with Spotify
+          Coming Soon
         </button>
       </div>
+      <div />
+      <div />
+      <div>
+        <h2
+          style={{
+            fontSize: "22px",
+            textTransform: "uppercase",
+            color: "black",
+            margin: "-5px 0 0 113px"
+          }}
+        >
+          a tempo driven playlist generator
+        </h2>
+      </div>
     </div>
+
+    {/* */}
   </>
 );
 
