0b4f492f8a2ec88b6c62b8b2da326db0d2859d8c
Jamie <jaylovelace42@gmail.com>
2020-09-06 23:35:01 +0100

    fix env

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,8 @@
-import env from "../env";
-
 const config = {
   SPOTIFY_API_URL: "https://api.spotify.com/v1/recommendations",
   SPOTIFY_AUTHORIZE_URL: "https://accounts.spotify.com/authorize",
   SPOTIFY_AUTH_SCOPES: "user-read-recently-played playlist-modify-public",
-  SPOTIFY_CLIENT_ID: env.SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_ID: process.env.REACT_APP_CLIENT,
   CALLBACK_URL: `${window.location.origin}/`,
 };
 
